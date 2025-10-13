ed490375151ae61e2f2069ae9f6b6ee8f956d97e
Maxxxel <m.gromball@gmx.de>
2025-10-13 09:17:54 +0200

    Release Package

--- sf_impex/addon_updater_ops.py
+++ sf_impex/addon_updater_ops.py
@@ -1355,12 +1355,12 @@ def register(bl_info):
 
     # Choose your own repository, must match git name for GitHUb and Bitbucket,
     # for GitLab use project ID (numbers only).
-    updater.repo = "SR-ImpEx-for-Blender"
+    updater.repo = "SF2-ImpEx-for-Blender"
 
     # updater.addon = # define at top of module, MUST be done first
 
     # Website for manual addon download, optional but recommended to set.
-    updater.website = "https://github.com/Maxxxel/SR-ImpEx-for-Blender.git"
+    updater.website = "https://github.com/Maxxxel/SF2-ImpEx-for-Blender.git"
 
     # Addon subfolder path.
     # "sample/path/to/addon"
